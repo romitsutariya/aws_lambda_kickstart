96dc77e5aae805eb84434a9041b98be1bd879f6c
romitsutariya <romitsutariya@gmail.com>
2026-04-05 08:52:05 +0530

    Mock lambda logger in handler test

--- src/test/java/org/example/LambdaHandlerTest.java
+++ src/test/java/org/example/LambdaHandlerTest.java
@@ -2,8 +2,10 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import org.junit.jupiter.api.Test;
 
 class LambdaHandlerTest {
@@ -12,7 +14,9 @@ class LambdaHandlerTest {
   @Test
   void handleRequestReturnsGreetingForAnimalSpecies() {
     Context context = mock(Context.class);
+    LambdaLogger logger = mock(LambdaLogger.class);
     Animal animal = new Animal("Rabbit", true, false);
+    when(context.getLogger()).thenReturn(logger);
 
     String response = lambdaHandler.handleRequest(animal, context);
 
